8195f166f29e074da4934081f1c6cef77e38eba8
VeKiMa <149431810+VeKiMa@users.noreply.github.com>
2023-12-14 20:35:40 +0100

    Kodna stranica

--- edunovawp3.sql
+++ edunovawp3.sql
@@ -3,7 +3,7 @@ go
 drop database if exists edunovawp3;
 go
 -- ovo je komentar
-create database edunovawp3;
+create database edunovawp3 collate Croatian_CI_AS;
 go
 --drop database edunovawp3;
 --use master
