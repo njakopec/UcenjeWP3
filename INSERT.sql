709e2eb019f9c38557f3f191ad21987ce5f3eeaa
VeKiMa <149431810+VeKiMa@users.noreply.github.com>
2023-12-14 20:58:41 +0100

    Unos podataka u tablicu smjer

---
+++ INSERT.sql
@@ -0,0 +1,11 @@
+use edunovawp3;
+
+select * from smjerovi;
+
+insert into smjerovi (naziv,trajanje,cijena,vaucer)
+values ('Web programiranje', 225, 1859.45, 1);
+
+insert into smjerovi values
+('Web dizajn',null,null,null);
+
+insert into smjerovi (naziv) values ('čćšžđ ČĆŠŽĐ')
