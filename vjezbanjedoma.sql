b0af6a95f9c54c165bd4baf40c32a7b9a40387e5
njakopec <153132341+njakopec@users.noreply.github.com>
2023-12-18 20:03:37 +0100

    vježbala kreiranje baze i unos

---
+++ vjezbanjedoma.sql
@@ -0,0 +1,82 @@
+﻿use master;
+go
+drop database if exists edunovawp3;
+go
+
+create database edunovawp3 collate Croatian_CI_AS;
+go
+
+use edunovawp3;
+
+create table smjerovi(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+trajanje int null, 
+cijena decimal(18,2), 
+vaucer bit
+);
+
+create table grupe(
+
+	sifra			int			not null primary key identity(1,1),
+	naziv			varchar(5)	not null,
+	smjer			int			not null,
+	datumpocetka	datetime,
+	maxpolaznika	int			not null,
+	predavac		int
+
+);
+
+
+create table polaznici(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+email varchar(100),
+oib char(11),
+brojugovora varchar(10)
+);
+
+create table predavaci(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+email varchar(100) not null,
+oib char(11),
+iban varchar(50)
+);
+
+
+
+
+create table clanovi(
+grupa int not null,
+polaznik int not null
+);
+
+
+alter table grupe add foreign key (smjer) references smjerovi(sifra);
+alter table grupe add foreign key (predavac) references predavaci(sifra);
+
+alter table clanovi add foreign key (grupa) references grupe(sifra);
+alter table clanovi add foreign key (polaznik) references polaznici(sifra);
+
+
+
+
+
+
+
+
+
+
+insert into smjerovi (naziv, trajanje,cijena,vaucer)
+values ('Web programiranje', 225, 1859.45,1);
+
+insert into smjerovi values
+('Web dizajn',null,null,null);
+
+
+insert into smjerovi (naziv) values ('čšćđž ČŠĆĐŽ');
+
+select * from smjerovi
